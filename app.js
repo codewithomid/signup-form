327556aaac4cb887df2fe9a4dc68dabd464baedd
Code With Omid <119259122+codewithomid@users.noreply.github.com>
2023-01-19 17:13:54 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    document.querySelector('.container').classList.toggle('change');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
